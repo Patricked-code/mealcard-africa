7adc540a2f5029577c1cf65ef2139b9c8802fbe3
Patricked-code <eric.diby@wealthtechinnovations.com>
2026-05-08 22:21:34 +0200

    Add navigation component

---
+++ apps/web/components/Nav.tsx
@@ -0,0 +1,3 @@
+export function Nav() {
+  return <div className="nav"><strong>MealCard Africa</strong><div className="links"><a href="/company">Entreprise</a><a href="/employee">Salarié</a><a href="/merchant">Marchand</a><a href="/admin">Admin</a><a className="btn primary" href="/login">Connexion</a></div></div>;
+}
